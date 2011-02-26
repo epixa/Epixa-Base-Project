82ab81067634c200ceba6f2881e45feb5352fe81
Court Ewing <court@epixa.com>
2011-02-26 17:45:23 -0500

    Base application structure

---
+++ application/core/src/Controller/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Epixa - Epixa-Base-Project
+ */
+
+namespace Core\Controller;
+
+use Epixa\Controller\AbstractController;
+
+/**
+ * Default controller
+ *
+ * @category   Module
+ * @package    Core
+ * @subpackage Controller
+ * @copyright  2011 epixa.com - Court Ewing
+ * @license    http://github.com/epixa/Epixa-Base-Project/blob/master/LICENSE New BSD
+ * @author     Court Ewing (court@epixa.com)
+ */
+class IndexController extends AbstractController
+{
+    public function indexAction()
+    {}
+}
---
+++ application/user/src/Repository/User.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Epixa - Epixa-Base-Project
+ */
+
+namespace User\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @category   Module
+ * @package    User
+ * @subpackage Repository
+ * @copyright  2011 epixa.com - Court Ewing
+ * @license    http://github.com/epixa/Epixa-Base-Project/blob/master/LICENSE New BSD
+ * @author     Court Ewing (court@epixa.com)
+ */
+class User extends EntityRepository
+{
+}
